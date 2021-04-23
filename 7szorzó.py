3aa6c5d48e00355c38c702b75714e452a8977e7b
nagymilan8231 <nagymilan-2020-c@cserhati-edu.hu>
2021-04-23 22:29:10 +0200

    késsz

---
+++ 7szorzó.py
@@ -0,0 +1,10 @@
+print("bnfdhvbdfbhb")
+cv:int=0
+while cv < 20:
+    cv +=1
+    b = (7*cv)
+    if b % 3 == 0:
+        print(str(b)+"*")
+    else:
+        print(str(b))
+        
---
+++ 7szorzóford.py
@@ -0,0 +1,10 @@
+print("bnfdhvbdfbhb")
+while True:
+    a = input("Add meg a karaktersorozatot! :")
+    b = input("Add meg a keresendő karaktert ! :")
+    if b in a:
+        break
+    print("Nincs benne ilyen karakter.")
+    print("Te a "+ b +"karaktert kerested, és ez van benne: ")
+
+
